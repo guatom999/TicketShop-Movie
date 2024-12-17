f0bb067452a8b7738dde63835d21352ae2441888
guatom999 <35306045+guatom999@users.noreply.github.com>
2024-12-17 17:57:11 +0700

    update parse jwt token method

--- pkg/jwtauth/jwtauth.go
+++ pkg/jwtauth/jwtauth.go
@@ -1,9 +1,13 @@
 package jwtauth
 
-import "github.com/golang-jwt/jwt/v4"
+import (
+	"errors"
+
+	"github.com/golang-jwt/jwt/v4"
+)
 
 type (
-	customerClaims struct {
+	CustomerClaims struct {
 		Id      string `json:"customer_id"`
 		Email   string `json:"email"`
 		UserNam string `json:"username"`
@@ -11,9 +15,31 @@ type (
 	}
 )
 
-// func NewAccessToken(secret string, expireAt int64, claims *customerClaims) string {
+func ParseToken(secret string, tokenString string) (*CustomerClaims, error) {
 
-// 	// claims := &customerClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &CustomerClaims{}, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("error: unexpected signing method")
+		}
 
-// 	return ""
-// }
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenMalformed) {
+			return nil, errors.New("error: token format is invalid")
+		} else if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, errors.New("error: token is expired")
+		} else {
+			return nil, errors.New("error: token is invalid")
+		}
+	}
+
+	if claims, ok := token.Claims.(*CustomerClaims); ok {
+		return claims, nil
+	} else {
+		return nil, errors.New("error: claims type is invalid")
+
+	}
+
+}
